f0d48cf3d36d81e65738e071478df573bce78262
mizu-pumpkin <marisolisabel.zucca@um.es>
2021-06-01 18:18:47 +0200

    Implemented activity detection. Object segmentation is wonky.

--- actividad.py
+++ actividad.py
@@ -2,18 +2,15 @@
 # # ACTIVIDAD. Construye un detector de movimiento en una región de interés
 # de la imagen marcada manualmente.
 # Guarda 2 ó 3 segundos de la secuencia detectada en un archivo de vídeo.
-# Opcional: muestra el objeto seleccionado anulando el fondo.
+# TODO: Opcional: muestra el objeto seleccionado anulando el fondo.
 
 import cv2 as cv
+import numpy as np
 from umucv.stream import autoStream
 from umucv.util import Video, ROI, putText
 
-def condition(key):
-    return key == ord('g')
-
 video = Video(fps=25)
-# Podemos activar o desactivar la grabación con video.ON
-#video.ON = True
+video.ON = False
 
 mainWindowName = 'input'
 cv.namedWindow(mainWindowName)
@@ -22,28 +19,48 @@ cv.moveWindow(mainWindowName, 0, 0)
 region = ROI(mainWindowName)
 
 bgsub = cv.createBackgroundSubtractorMOG2(500, 16, False)
+kernel = np.ones((3,3),np.uint8)
 
 for key, frame in autoStream():
+
+    if key == ord('g'):
+        video.ON = not video.ON
+    
+    #frame = cv.flip(frame, 1)
     
     if region.roi:
-        # Mostrar el ROI seleccionado en una nueva ventana
+        # Select ROI
         [x1,y1,x2,y2] = region.roi
-        trozo = frame[y1:y2+1, x1:x2+1]
-        cv.imshow('actividad', trozo)
-        cv.imshow('mask', bgsub.apply(trozo) )
-        # Grabar según condición
-        if condition(key):
-            video.write(trozo)
-        # Resetear el ROI
+        act_roi = frame[y1:y2+1, x1:x2+1]
+
+        # Detect activity
+        # Remove noise from ROI
+        noiseless = cv.cvtColor(act_roi, cv.COLOR_BGR2GRAY)
+        noiseless = cv.GaussianBlur(noiseless, (21, 21), 0)
+        # Detected activity mask
+        fgmask = bgsub.apply(noiseless)
+        fgmask = cv.erode(fgmask,kernel,iterations = 1)
+        fgmask = cv.medianBlur(fgmask,3)
+        # Detected object
+        obj = act_roi.copy()
+        obj[fgmask==0] = 0
+        cv.imshow('Activity', obj)
+
+        # Write video if any activity is detected
+        if fgmask.any():
+            video.write(act_roi)
+            putText(frame, 'Activity detected', orig=(x1,y1-8))
+            if video.ON: cv.circle(act_roi,(15,15),6,(0,0,255),-1)
+
+        # Draw ROI rectangle on frame
+        cv.rectangle(frame, (x1,y1), (x2,y2), color=(0,255,255), thickness=2)
+
+        # Reset ROI
         if key == ord('x'):
             region.roi = []
-            cv.destroyWindow('actividad')
-        # Dibujar el ROI en el frame principal
-        cv.rectangle(frame, (x1,y1), (x2,y2), color=(0,255,255), thickness=2)
-        putText(frame, f'{x2-x1+1}x{y2-y1+1}', orig=(x1,y1-8))
+            cv.destroyWindow('Activity')
 
-    h,w,_ = frame.shape
-    putText(frame, f'{w}x{h}')
+    putText(frame, 'Video record: '+('ON' if video.ON else 'OFF') )
     cv.imshow(mainWindowName,frame)
 
 cv.destroyAllWindows()
