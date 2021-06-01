be03658ac636866a40c73ca3cc27c36b56a672c4
mizu-pumpkin <marisolisabel.zucca@um.es>
2021-06-01 19:55:20 +0200

    Loop to detect ROI

--- filtros.py
+++ filtros.py
@@ -6,3 +6,37 @@
 # Opcional: implementa en Python o C "desde cero" algún filtro y compara la
 # eficiencia con OpenCV.
 
+import cv2 as cv
+import numpy as np
+from umucv.stream import autoStream
+from umucv.util import ROI, putText
+
+mainWindowName = 'input'
+cv.namedWindow(mainWindowName)
+cv.moveWindow(mainWindowName, 0, 0)
+
+region = ROI(mainWindowName)
+
+for key, frame in autoStream():
+    
+    frame = cv.flip(frame, 1)
+    
+    if region.roi:
+        # Select ROI
+        [x1,y1,x2,y2] = region.roi
+        roi = frame[y1:y2+1, x1:x2+1]
+        cv.imshow('ROI', roi)
+
+        # Draw ROI rectangle on frame
+        cv.rectangle(frame, (x1,y1), (x2,y2), color=(0,255,255), thickness=2)
+
+        # Reset ROI
+        if key == ord('x'):
+            region.roi = []
+            cv.destroyWindow('ROI')
+
+    h,w,_ = frame.shape
+    putText(frame, f'({w},{h})' )
+    cv.imshow(mainWindowName,frame)
+
+cv.destroyAllWindows()
